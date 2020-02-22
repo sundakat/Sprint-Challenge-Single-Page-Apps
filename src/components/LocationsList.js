3d10dc1fe6cf5c5209914a391b63b1e52eef9621
Frances Hampton <bethhampton@Francess-MBP.attlocal.net>
2020-02-21 22:31:23 -0600

    LocationsList.js done

--- src/components/LocationsList.js
+++ src/components/LocationsList.js
@@ -1,3 +1,31 @@
 import React, { useEffect, useState } from "react";
+import axios from 'axios';
+import LocationCard from "../components/LocationCard";
 
-export default function LocationsList() {}
+export default function LocationsList() {
+
+
+    const [location, setLocation] = useState([])
+
+    useEffect(() => {
+        axios.get("https://rickandmortyapi.com/api/location/").then(res => {
+            console.log(res.data.results);
+            setLocation(res.data.results);
+        }).catch(e => console.log(e));
+    }, []);
+
+    return (
+        <section>
+            {location.map(location =>
+                <LocationCard
+                    id={location.id}
+                    name={location.name}
+                    type={location.type}
+                    dimension={location.dimension}
+                    residents={location.residents}
+                />
+            )
+            }
+        </section>
+    );
+}

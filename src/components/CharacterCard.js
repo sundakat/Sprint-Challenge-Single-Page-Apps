f5663f526ccef0d57f0c47f9887bceb48ccc4053
Frances Hampton <bethhampton@Francess-MBP.attlocal.net>
2020-02-21 22:27:26 -0600

    CharacterCard.js done

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,5 +1,26 @@
 import React from "react";
+import styled from "styled-components";
 
-export default function CharacterCard() {
-  return <span>todo: character</span>;
+const Man = styled.div`
+font-size:18px;
+  border:1px solid black;
+  text-align:center;
+  border-radius:10px;
+  padding:5px;
+  margin: 15px;
+  color:black;
+  background:#a9d3e9;
+  padding:2%;
+`;
+
+export default function CharacterCard(props) {
+  return (
+    <Man>
+      <h2>{props.name}</h2>
+      <p>Status: {props.status}</p>
+      <p>Gender: {props.gender}</p>
+      <p>Species: {props.species}</p>
+      <img src={props.image} />
+    </Man>
+  )
 }
